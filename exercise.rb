5f7aec96648a4f9a61f233dac7888d386631884c
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-30 13:57:48 -0400

    resolved bug by adding while loop

---
+++ exercise.rb
@@ -0,0 +1,25 @@
+require 'pry'
+def select_cards(possible_cards, hand)
+  i=0
+  while i < possible_cards.size
+  possible_cards.each do |current_card|
+    print "Do you want to pick up #{current_card}?"
+    answer = gets.chomp
+    if answer.downcase == 'y'
+      hand << current_card
+      i += 1
+    end
+    end
+    # binding.pry
+    return hand
+  end
+
+end
+
+available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of spades', 'queen of hearts']
+
+new_hand = select_cards(available_cards, [])
+
+display_hand = new_hand.join("\n")
+
+puts "Your new hand is: \n#{display_hand}"
